d195c84a23eaed9602ab6bfbb94dfc4f42d2dba6
ultra-noob <80683578+ultra-noob@users.noreply.github.com>
2021-08-11 20:57:31 +0530

    Update trackAdd.js

--- player-events/trackAdd.js
+++ player-events/trackAdd.js
@@ -3,7 +3,7 @@ module.exports = (client, message, queue, track) => {
         embed: {
           color: '00d9ff',
             description: `${client.emotes.naa} | ${track.title} has been added to the queue !`,
-            footer: { text: `© UPlay 2020 | Requested by ${message.author.username}` },
+            footer: { text: `©®eated by Nikhil | Requested by ${message.author.username}` },
         },
     });
 };
