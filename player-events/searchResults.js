e29e586256835801e5ecf252dfba007726939010
ultra-noob <80683578+ultra-noob@users.noreply.github.com>
2021-08-11 21:01:18 +0530

    Update searchResults.js

--- player-events/searchResults.js
+++ player-events/searchResults.js
@@ -3,7 +3,7 @@ module.exports = (client, message, query, tracks) => {
         embed: {
             color: "00d9ff",
             author: { name: `Here are your search results for ${query}` },
-            footer: { text: `© UPlay 2020 | Requested by ${message.author.username}` },
+            footer: { text: `©®eated by Nikhil | Requested by ${message.author.username}` },
             timestamp: new Date(),
             description: `${tracks.map((t, i) => `**${i + 1}** - ${t.title}`).join('\n')}`,
         },
