a17ff91bfe420b529db39895f8d7c4dbd5d65a59
ultra-noob <80683578+ultra-noob@users.noreply.github.com>
2021-08-11 20:40:59 +0530

    Update trackStart.js

--- player-events/trackStart.js
+++ player-events/trackStart.js
@@ -3,7 +3,7 @@ module.exports = (client, message, track) => {
         embed: {
           color: "00d9ff",
             description: `${client.emotes.naa} | Now playing ${track.title} into ${message.member.voice.channel.name} will Now play.`,
-            footer: { text: `© UPlay 2020 | Requested by ${message.author.username}` },
+            footer: { text: `©®eated by Nikhil  | Requested by ${message.author.username}` },
         },
     });
 };
